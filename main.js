35b24826478606fd3c6b3b9b3e8714d79d3313af
Selt0 <chewedgum12@gmail.com>
2019-12-15 14:33:48 -0600

    Create button for palindrome testing

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 let Phrase = require("selt0-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing: ");
+function palindromeDetector() {
+  let string = prompt("Please enter a string for palindrome testing: ");
 
-let phrase = new Phrase(string);
+  let phrase = new Phrase(string);
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is NOT a palindrome.`);
+  if (phrase.palindrome()) {
+    alert(`"${phrase.content}" is a palindrome!`);
+  } else {
+    alert(`"${phrase.content}" is NOT a palindrome.`);
+  }
 }
+
+let button = document.querySelector('#palindromeDetector');
+button.addEventListener('click', function() {
+  palindromeDetector();
+});
